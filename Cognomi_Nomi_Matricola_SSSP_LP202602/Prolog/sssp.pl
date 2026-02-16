ef27bb8ac646f850497ab33fac9cdd90d6bf1956
Stome <lor.stomeo@gmail.com>
2026-02-16 19:02:45 +0100

    Prime modifiche

---
+++ Cognomi_Nomi_Matricola_SSSP_LP202602/Prolog/sssp.pl
@@ -0,0 +1,36 @@
+:- dynamic graph/1.
+:- dynamic vertex/2.
+
+
+new_graph(G) :-
+    graph(G),     
+    !.         
+
+new_graph(G) :-  
+    assert(graph(G)), 
+    !.
+
+delete_graph(G) :-
+    retractall(vertex(G, _)),
+    
+    retractall(arc(G, _, _, _)),
+    
+    retractall(distance(G, _, _)),
+    retractall(visited(G, _)),
+    retractall(previous(G, _, _)),
+    
+    retractall(graph(G)).
+    
+new_vertex(G, V) :-
+    vertex(G, V),
+    !.
+
+new_vertex(G, V) :-
+    graph(G),             
+    assert(vertex(G, V)), 
+    !.
+
+vertices(G, Vs) :-
+    findall(V, vertex(G, V), Vs).
+
+list_vertices(G) :- listing(vertex(G, _)).
