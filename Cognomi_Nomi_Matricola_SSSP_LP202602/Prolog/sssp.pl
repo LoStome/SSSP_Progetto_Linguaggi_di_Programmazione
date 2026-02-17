78d3b3570050a48b8cc207c80fdeb5d8140c9151
Stome <lor.stomeo@gmail.com>
2026-02-17 12:32:07 +0100

    finita prima parte su 3 prolog

--- Cognomi_Nomi_Matricola_SSSP_LP202602/Prolog/sssp.pl
+++ Cognomi_Nomi_Matricola_SSSP_LP202602/Prolog/sssp.pl
@@ -1,5 +1,6 @@
 :- dynamic graph/1.
 :- dynamic vertex/2.
+:- dynamic arc/3.
 :- dynamic arc/4.
 
 new_graph(G) :-
@@ -33,13 +34,13 @@ new_vertex(G, V) :-
 vertices(G, Vs) :-
     findall(V, vertex(G, V), Vs).
 
-
 list_vertices(G) :-
     listing(vertex(G, _)).
 
 
-new_arc(G, U, V) :-
-    new_arc(G, U, V, 1).
+
+
+new_arc(G, U, V) :- new_arc(G, U, V, 1).
 
 
 new_arc(G, U, V, _) :-
@@ -48,9 +49,26 @@ new_arc(G, U, V, _) :-
 
 
 new_arc(G, U, V, Weight) :-
-    graph(G),                    
-    vertex(G, U),               
-    vertex(G, V),              
-    Weight >= 0,                
+    Weight >= 0,          
+    
+    new_graph(G),        
+    new_vertex(G, U),     
+    new_vertex(G, V),      
+    
     assert(arc(G, U, V, Weight)),
     !.
+
+arcs(G, Es) :-
+    findall(arc(G, U, V, Weight), arc(G, U, V, Weight), Es).
+
+
+neighbors(G, V, Ns) :-
+    vertex(G, V),                                   
+    findall(arc(G, V, N, W), arc(G, V, N, W), Ns).  
+
+list_arcs(G) :-
+    listing(arc(G, _, _, _)).
+
+list_graph(G) :-
+    list_vertices(G),
+    list_arcs(G).
